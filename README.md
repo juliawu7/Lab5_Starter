700842d2d4d9dd170aea5140b9b55c0e69963df9
juliawu7 <juliaww73@gmail.com>
2025-05-06 13:56:32 -0700

    finished part 1 of explore and readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 Julia Wu and Mei Man Teng
+1. Yes, you would use a unit test to test the "message" feature of a messaging application, but only to verify small, specific functionalities, such as rejecting empty messages or enforcing a character limit.
+2. Yes, you would use a unit test to test the "max message length" feature of a messaging application since it is also a small, specific functionality. Additionally, given some input, in this case, the message, it should always give the same result (whether or not it is more than 80 characters).
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,46 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const textInput = document.getElementById("text-to-speak");
+  const voiceSelect = document.getElementById("voice-select");
+  const talkButton = document.querySelector("button");
+  const faceImage = document.querySelector("#explore img");
+
+  const synth = window.speechSynthesis;
+  let voices = [];
+
+  function populateVoices() {
+    voices = synth.getVoices();
+    voices.forEach((voice) => {
+      const option = document.createElement("option");
+      option.value = voice.name;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      voiceSelect.appendChild(option);
+    });
+  }
+  populateVoices();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+  talkButton.addEventListener("click", () => {
+    const utterance = new SpeechSynthesisUtterance(textInput.value);
+    const selectedVoiceName = voiceSelect.value;
+    const selectedVoice = voices.find(v => v.name === selectedVoiceName);
+    if (selectedVoice) {
+      utterance.voice = selectedVoice;
+    }
+    utterance.addEventListener("start", () => {
+      faceImage.src = "assets/images/smiling-open.png";
+      faceImage.alt = "Open smiling face";
+    }
+    );
+    utterance.addEventListener("end", () => {
+      faceImage.src = "assets/images/smiling.png";
+      faceImage.alt = "Closed smiling face";
+    }
+    );
+    synth.speak(utterance);
+  });
+
 }
